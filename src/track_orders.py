90cd57c64771aa58ff64287b1c6fdafe26ed3c85
joaokeuwe <joaov2872@hotmail.com>
2022-10-30 00:42:41 -0300

    fix: :bug: tem que irrrr

--- src/track_orders.py
+++ src/track_orders.py
@@ -10,8 +10,8 @@ class TrackOrders:
 
     def get_most_ordered_dish_per_customer(self, customer):
         foods = {}
-        for customer, comida, _ in self.data:
-            if customer == customer:
+        for cliente, comida, _ in self.data:
+            if cliente == customer:
                 if comida not in foods:
                     foods[comida] = 0
                 foods[comida] += 1
@@ -21,12 +21,30 @@ class TrackOrders:
     def get_never_ordered_per_customer(self, customer):
         total = set()
         food = set()
-        for customer, food, _ in self.data:
+        for cliente, food, _ in self.data:
             total.add(food)
-            if customer == customer:
+            if cliente == customer:
                 food.add(food)
                 reponse = total.difference(food)
                 return reponse
+
+    def get_orders_per_customer(self, customer, order):
+        orders = 0
+        for client, food, _day in self.data:
+            if client == customer and order == food:
+                orders += 1
+        return orders
+    
+    
+    def get_days_never_visited_per_customer(self, customer):
+        totalDias = set()
+        dias = set()
+        for cliente, _, day in self.data:
+            totalDias.add(day)
+            if cliente == customer:
+                dias.add(day)
+        response = totalDias.difference(dias)
+        return response
     
     def get_busiest_day(self):
         dias = {}
@@ -37,5 +55,21 @@ class TrackOrders:
         response = max(dias, key=dias.get)
         return response
 
+    def get_least_busy_day(self):
+        dias = {}
+        for _cust, _order, days in self.data:
+            if days not in dias:
+                dias[days] = 0
+            dias[days] += 1
+
+        response = min(dias, key=dias.get)
+        return response
+
+    def get_orders_per_customer(self, customer, order):
+        pedido = 0
+        for cliente, food, _day in self.data:
+            if cliente == customer and order == food:
+                pedido += 1
+        return pedido
 
    
