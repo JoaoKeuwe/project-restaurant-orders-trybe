ff85c77142d5c3402e9c883607ff39a1fba0c24a
joaokeuwe <joaov2872@hotmail.com>
2022-10-30 00:20:57 -0300

    fix: :fire: refazendo o projeto de uma forma difernte pra ver se passa

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,8 +1,29 @@
+import csv
+
+from src.track_orders import TrackOrders
+
+
 def analyze_log(path_to_file):
-    if not path_to_file.endswith(".csv"):
-        raise FileNotFoundError(f'Extensão inválida: {path_to_file}')
+    if not str(path_to_file).endswith('.csv'):
+        raise FileNotFoundError(f"Extensão inválida: {path_to_file}")
     try:
-        with open(path_to_file, encoding="utf-8") as arquivo:
-            print(arquivo)
+        with open(path_to_file, encoding="utf8") as arquivo:
+            reader = csv.reader(
+                arquivo, delimiter=",", quotechar='"'
+            )
+            *data, = reader
+        order = TrackOrders()
+        for client, food, day in data:
+            order.add_new_order(client, food, day)
+        answer1 = order.get_most_ordered_dish_per_customer("maria")
+        answer2 = order.get_order_per_customer("arnaldo", "hamburguer")
+        answer3 = order.get_never_ordered_per_customer("joao")
+        answer4 = order.get_days_never_visited_per_customer("joao")
+        with open("data/mkt_campaign.txt", mode="w") as file:
+            file.write(
+                f'{answer1}\n{answer2}\n{answer3}\n{answer4}'
+            )
     except FileNotFoundError:
-        raise FileNotFoundError(f'Arquivo inexistente: {path_to_file}')
+        raise FileNotFoundError(f"Arquivo inexistente: {path_to_file}")
+    
+    # refazendo o projeto diferente pois da outra maneira não passava
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,34 +1,19 @@
 class TrackOrders:
     def __init__(self):
-        lists = list()
-        self.myList = lists
-   
-    # aqui deve expor a quantidade de estoque
+        self.data = []
+
     def __len__(self):
-        return len(self.myList)
+        return len(self.data)
 
     def add_new_order(self, customer, order, day):
-        self.myList.append({day, order, customer})
+        self.data.append((customer, order, day))
 
     def get_most_ordered_dish_per_customer(self, customer):
-        list = [orders[1] for orders in self.myList
-                if orders[2] == customer]
-        return max(list, key=list.count)
-
-    def get_never_ordered_per_customer(self, customer):
-        foods = [orders[1] for orders in self.myList]
-        foodsCustomer = [orders[1] for orders in self.myList
-                         if customer == orders[2]]
-
-        return set(set(foods) - set(foodsCustomer))
-
-    def get_days_never_visited_per_customer(self, customer):
-        pass
-
-    def get_busiest_day(self):
-        list = [orders[0] for orders in self.myList]
-        return max(list, key=list.count)
-
-    def get_least_busy_day(self):
-        list = [orders[0] for orders in self.minha_list]
-        return min(list, key=list.count)
+        foods = {}
+        for customer, comida, _ in self.data:
+            if customer == customer:
+                if comida not in foods:
+                    foods[comida] = 0
+                foods[comida] += 1
+        response = max(foods, key=foods.get)
+        return response
