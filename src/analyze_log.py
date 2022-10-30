66ffd7b841d7f67da6cb5b23002ed13cde3a9b38
joaokeuwe <joaov2872@hotmail.com>
2022-10-30 00:09:26 -0300

    feat:

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -2,7 +2,7 @@ def analyze_log(path_to_file):
     if not path_to_file.endswith(".csv"):
         raise FileNotFoundError(f'Extensão inválida: {path_to_file}')
     try:
-        with open(path_to_file, encoding="utf-8") as pasta:
-            print(pasta)
+        with open(path_to_file, encoding="utf-8") as arquivo:
+            print(arquivo)
     except FileNotFoundError:
         raise FileNotFoundError(f'Arquivo inexistente: {path_to_file}')
